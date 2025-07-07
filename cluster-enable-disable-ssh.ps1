b71e8e0d573cfe3b44329cbe1d8fb7be949d52e7
Alexey <aa.koznov@vmadm.in>
2025-07-07 10:39:16 +0200

    Update and rename cluster-enable-ssh.ps1 to cluster-enable-disable-ssh.ps1

--- cluster-enable-disable-ssh.ps1
+++ cluster-enable-disable-ssh.ps1
@@ -24,3 +24,29 @@ Write-Host "SSH has been enabled on all hosts in cluster: $ClusterName" -Foregro
 
 # Disconnect from vCenter
 Disconnect-VIServer -Confirm:$false
+
+
+
+# Get all ESXi hosts in the cluster
+$ClusterName = "Your-Cluster-Name"
+$ESXiHosts = Get-Cluster -Name $ClusterName | Get-VMHost
+
+# Disable SSH service on all hosts in the cluster
+foreach ($VMHost in $ESXiHosts) {
+    Write-Host "Disabling SSH on $($VMHost.Name)..." -ForegroundColor Red
+    
+    # Stop SSH service
+    Get-VMHostService -VMHost $VMHost | Where-Object {$_.Key -eq "TSM-SSH"} | Stop-VMHostService -Confirm:$false
+    
+    # Set SSH service to manual start (off)
+    Get-VMHostService -VMHost $VMHost | Where-Object {$_.Key -eq "TSM-SSH"} | Set-VMHostService -Policy "Off"
+    
+    # Verify SSH is stopped
+    $SSHStatus = Get-VMHostService -VMHost $VMHost | Where-Object {$_.Key -eq "TSM-SSH"}
+    Write-Host "SSH Status on $($VMHost.Name): $($SSHStatus.Running) (Policy: $($SSHStatus.Policy))" -ForegroundColor Yellow
+}
+
+Write-Host "SSH has been disabled on all hosts in cluster: $ClusterName" -ForegroundColor Red
+
+# Disconnect from vCenter
+Disconnect-VIServer -Confirm:$false
